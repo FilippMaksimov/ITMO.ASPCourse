9661fbc64f3ed12df9f45eafab7d55cf62f39d0f
FilippMaksimov <116656010+FilippMaksimov@users.noreply.github.com>
2023-02-21 02:26:02 +0300

    Background and theme Update

---
+++ ITMO.ASPCourse.MVC.Lab08/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.ASPCourse.MVC.Lab08.MvcApplication" Language="C#" %>
---
+++ ITMO.ASPCourse.MVC.Lab08/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITMO.ASPCourse.MVC.Lab08.Startup))]
+namespace ITMO.ASPCourse.MVC.Lab08
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
